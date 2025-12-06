7d8a7d4b3f3df778a9cce9ae649ef171da09ce0c
Marc <marc11552001@gmail.com>
2025-12-06 04:31:00 +0100

    fix: use absolute path for brand image

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,11 +1,19 @@
 
+// @ts-ignore
 import { addons } from '@storybook/manager-api';
+// @ts-ignore
 import { create } from '@storybook/theming';
 
 addons.setConfig({
     theme: create({
         base: 'light',
         brandTitle: "Marc's Design System",
+
+        // 👇 SOLUCIÓN:
+        // 1. Asegúrate de poner la extensión correcta (.png o .ico según tu archivo)
+        // 2. Añade '/design-system/' delante para que funcione en GitHub Pages
+        brandImage: '/design-system/favicon.png',
+
         brandUrl: 'https://github.com/IniguezMarc/design-system',
         brandTarget: '_self',
     }),
