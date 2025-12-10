cb7da0d767d4ac4d5ad24ebb88aab75fbfc7568a
IniguezMarc <miniguezbarbero@gmail.com>
2025-12-10 09:33:34 +0100

    fix: add missing ProjectCard wrapper for build

---
+++ src/components/molecules/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,4 @@
+import { BasicProjectCard, type BasicProjectCardProps } from './BasicProjectCard';
+
+export { BasicProjectCard as ProjectCard };
+export type { BasicProjectCardProps as ProjectCardProps };
