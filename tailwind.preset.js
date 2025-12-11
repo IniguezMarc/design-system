20405c175009799ed8a25537f0af99fc974e5148
IniguezMarc <miniguezbarbero@gmail.com>
2025-12-11 18:00:01 +0100

    feat: add tailwind.preset.js and bump v0.0.9

---
+++ tailwind.preset.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    safelist: [
+        {
+            pattern: /(bg|text|border)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-[0-9]{2,3}/,
+            variants: ['hover', 'focus', 'dark', 'dark:hover', 'dark:focus'],
+        }
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
